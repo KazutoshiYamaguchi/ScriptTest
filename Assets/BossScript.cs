87b7a7c11abcbc7ca04cae12ba08477300cff56a
KazutoshiYamaguchi <umaibou300pon@icloud.com>
2020-07-27 16:28:23 +0900

    ScriptTest 初めてのスクリプト作成
    配列の各要素を順番に表示、また逆順に表示しました。

---
+++ Assets/BossScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossScript : MonoBehaviour
+{
+
+
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+		Boss magic = new Boss();
+
+		magic.Magic();
+		magic.Magic();
+		magic.Magic();
+		magic.Magic();
+		magic.Magic();
+		magic.Magic();
+		magic.Magic();
+		magic.Magic();
+		magic.Magic();
+		magic.Magic();
+		magic.Magic();//11回目
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+
+	public class Boss
+	{
+		private int mp = 53;
+
+		public void Magic()
+		{
+			mp -= 5;
+
+			if (mp > 0)
+			{
+
+				Debug.Log("魔法攻撃をした。残りMPは" + this.mp);
+			}
+			else
+			{
+				Debug.Log("MPが足りないため魔法が使えない。");
+
+			}
+		}
+	}
+}
---
+++ Assets/Task1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task1 : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+		int[] array = new int[5];
+
+		array[0] = 1;
+		array[1] = 12;
+		array[2] = 11;
+		array[3] = 41;
+		array[4] = 14;
+
+		for (int i = 0; i < 5; i++)
+		{
+			Debug.Log(array[i]);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
